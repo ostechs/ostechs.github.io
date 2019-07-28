830e5f90dcd7144bc9acaa5454f37f17e2533285
ostechs <ostechs@outlook.com>
2019-07-27 19:31:26 -0700

    Initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# ostechs.github.io
--- myjava.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("lastMod").innerHTML = document.lastModified;
-
-function myAlert() {
-    alert("Thank you for contacting us. We will be contacting you shortly.")
-}
